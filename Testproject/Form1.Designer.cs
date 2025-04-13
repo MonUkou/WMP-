e4c8ecdc311a4a8949b65768e29735fe26318dec
Monukou <Monukou666@gmail.com>
2025-04-13 21:21:14 +0700

    init

---
+++ Testproject/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Testproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.ListSong = new System.Windows.Forms.ListBox();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Remove = new System.Windows.Forms.Button();
+            this.btn_Swap = new System.Windows.Forms.Button();
+            this.lbl_track_ = new System.Windows.Forms.Label();
+            this.lbl_track_start = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lbl_volume = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.axWindowsMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.splitContainer1.Panel1.Controls.Add(this.ListSong);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Add);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Remove);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Swap);
+            this.splitContainer1.Panel1.ForeColor = System.Drawing.Color.Black;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lbl_track_);
+            this.splitContainer1.Panel2.Controls.Add(this.lbl_track_start);
+            this.splitContainer1.Panel2.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.axWindowsMediaPlayer);
+            this.splitContainer1.Size = new System.Drawing.Size(779, 449);
+            this.splitContainer1.SplitterDistance = 222;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // ListSong
+            // 
+            this.ListSong.AccessibleName = "";
+            this.ListSong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ListSong.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListSong.ForeColor = System.Drawing.Color.Aquamarine;
+            this.ListSong.FormattingEnabled = true;
+            this.ListSong.ItemHeight = 16;
+            this.ListSong.Location = new System.Drawing.Point(12, 7);
+            this.ListSong.Name = "ListSong";
+            this.ListSong.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.ListSong.Size = new System.Drawing.Size(199, 308);
+            this.ListSong.TabIndex = 0;
+            this.ListSong.DoubleClick += new System.EventHandler(this.ListSong_DoubleClick);
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.BackColor = System.Drawing.Color.Black;
+            this.btn_Add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.ForeColor = System.Drawing.Color.Aquamarine;
+            this.btn_Add.Location = new System.Drawing.Point(68, 325);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(87, 38);
+            this.btn_Add.TabIndex = 0;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = false;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Remove
+            // 
+            this.btn_Remove.BackColor = System.Drawing.Color.Black;
+            this.btn_Remove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Remove.ForeColor = System.Drawing.Color.Aquamarine;
+            this.btn_Remove.Location = new System.Drawing.Point(68, 369);
+            this.btn_Remove.Name = "btn_Remove";
+            this.btn_Remove.Size = new System.Drawing.Size(87, 36);
+            this.btn_Remove.TabIndex = 1;
+            this.btn_Remove.Text = "Remove";
+            this.btn_Remove.UseVisualStyleBackColor = false;
+            this.btn_Remove.Click += new System.EventHandler(this.btn_Remove_Click);
+            // 
+            // btn_Swap
+            // 
+            this.btn_Swap.BackColor = System.Drawing.Color.Black;
+            this.btn_Swap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Swap.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Swap.ForeColor = System.Drawing.Color.Aquamarine;
+            this.btn_Swap.Location = new System.Drawing.Point(68, 411);
+            this.btn_Swap.Name = "btn_Swap";
+            this.btn_Swap.Size = new System.Drawing.Size(87, 31);
+            this.btn_Swap.TabIndex = 2;
+            this.btn_Swap.Text = "Swap";
+            this.btn_Swap.UseVisualStyleBackColor = false;
+            this.btn_Swap.Click += new System.EventHandler(this.btn_Swap_Click);
+            // 
+            // lbl_track_
+            // 
+            this.lbl_track_.AutoSize = true;
+            this.lbl_track_.BackColor = System.Drawing.Color.Black;
+            this.lbl_track_.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_track_.ForeColor = System.Drawing.Color.Aquamarine;
+            this.lbl_track_.Location = new System.Drawing.Point(511, 308);
+            this.lbl_track_.Name = "lbl_track_";
+            this.lbl_track_.Size = new System.Drawing.Size(43, 16);
+            this.lbl_track_.TabIndex = 6;
+            this.lbl_track_.Text = "00:00";
+            // 
+            // lbl_track_start
+            // 
+            this.lbl_track_start.AutoSize = true;
+            this.lbl_track_start.BackColor = System.Drawing.Color.Black;
+            this.lbl_track_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_track_start.ForeColor = System.Drawing.Color.Aquamarine;
+            this.lbl_track_start.Location = new System.Drawing.Point(-3, 308);
+            this.lbl_track_start.Name = "lbl_track_start";
+            this.lbl_track_start.Size = new System.Drawing.Size(43, 16);
+            this.lbl_track_start.TabIndex = 5;
+            this.lbl_track_start.Text = "00:00";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(36, 308);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(481, 16);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Aquamarine;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Window Media Player";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Controls.Add(this.btnNext);
+            this.groupBox1.Controls.Add(this.btnPrev);
+            this.groupBox1.Controls.Add(this.btnPlay);
+            this.groupBox1.Controls.Add(this.lbl_volume);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Aquamarine;
+            this.groupBox1.Location = new System.Drawing.Point(-11, 325);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(565, 128);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control ";
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnNext.BackgroundImage")));
+            this.btnNext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnNext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Location = new System.Drawing.Point(331, 45);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Padding = new System.Windows.Forms.Padding(5);
+            this.btnNext.Size = new System.Drawing.Size(49, 35);
+            this.btnNext.TabIndex = 7;
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPrev.BackgroundImage")));
+            this.btnPrev.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPrev.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrev.Location = new System.Drawing.Point(203, 45);
+            this.btnPrev.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Padding = new System.Windows.Forms.Padding(5);
+            this.btnPrev.Size = new System.Drawing.Size(49, 35);
+            this.btnPrev.TabIndex = 6;
+            this.btnPrev.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPlay.BackgroundImage")));
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Location = new System.Drawing.Point(258, 35);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Padding = new System.Windows.Forms.Padding(5);
+            this.btnPlay.Size = new System.Drawing.Size(67, 45);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lbl_volume
+            // 
+            this.lbl_volume.AutoSize = true;
+            this.lbl_volume.BackColor = System.Drawing.Color.Black;
+            this.lbl_volume.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_volume.ForeColor = System.Drawing.Color.Aquamarine;
+            this.lbl_volume.Location = new System.Drawing.Point(500, 82);
+            this.lbl_volume.Name = "lbl_volume";
+            this.lbl_volume.Size = new System.Drawing.Size(28, 16);
+            this.lbl_volume.TabIndex = 4;
+            this.lbl_volume.Text = "0%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Aquamarine;
+            this.label3.Location = new System.Drawing.Point(489, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Volume";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(458, 10);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(56, 107);
+            this.trackBar1.TabIndex = 0;
+            // 
+            // axWindowsMediaPlayer
+            // 
+            this.axWindowsMediaPlayer.Enabled = true;
+            this.axWindowsMediaPlayer.Location = new System.Drawing.Point(-11, 3);
+            this.axWindowsMediaPlayer.Name = "axWindowsMediaPlayer";
+            this.axWindowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer.OcxState")));
+            this.axWindowsMediaPlayer.Size = new System.Drawing.Size(565, 321);
+            this.axWindowsMediaPlayer.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 449);
+            this.Controls.Add(this.splitContainer1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WindowMediaPlayer";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Swap;
+        private System.Windows.Forms.Button btn_Remove;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label lbl_volume;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox ListSong;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lbl_track_start;
+        private System.Windows.Forms.Label lbl_track_;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+    }
+}
+
---
+++ Testproject/Form1.cs
@@ -0,0 +1,130 @@
+﻿using AxWMPLib;
+using MusicPlayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Testproject
+{
+    public partial class Form1 : Form
+    {
+        private DoubleLinkedList DLL = new DoubleLinkedList();   //Khởi tạo danh sách liên kết
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OpenFileDialog openFileDialog;    //Khởi tạo biến để mở hộp thoại file cho phép chọn file
+        string[] filePaths, fileNames;    //Mảng chứa đường dẫn và tên file của bài hát
+        private void btn_Add_Click(object sender, EventArgs e)   
+        {
+            openFileDialog = new OpenFileDialog();   //Khởi tạo hộp thoại mở file
+            openFileDialog.Filter = "Audio and Video Files (*.mp3; *.mp4)|*.mp3; *.mp4";   //Chỉ cho phép chọn file âm thanh và video
+            openFileDialog.Multiselect = true;   //Cho phép chọn nhiều file
+            openFileDialog.CheckFileExists = true;   //Kiểm tra file tồn tại
+            openFileDialog.CheckPathExists = true;   //Kiểm tra đường dẫn tồn tại
+            openFileDialog.Title = "Select Audio or Video Files";   //Tiêu đề của hộp thoại
+            if (openFileDialog.ShowDialog() == DialogResult.OK)   //Nếu chọn file thành công
+            {
+                string[] filePaths = openFileDialog.FileNames;   //Lấy đường dẫn của các file
+                string[] fileNames = openFileDialog.SafeFileNames;   //Lấy tên của các file
+                for (int i = 0; i < filePaths.Length; i++)   //Duyệt qua các file đã chọn
+                {
+                    bool isExist = false;   //Biến kiểm tra file đã tồn tại trong danh sách chưa
+                    foreach (string item in ListSong.Items)   //Duyệt qua các item trong danh sách
+                    {
+                        if (item.ToString().Equals(fileNames[i], StringComparison.OrdinalIgnoreCase))   //Nếu file đã tồn tại trong danh sách (không phân biệt hoa thường)
+                        {
+                            isExist = true;   //Gán biến kiểm tra bằng true
+                            break;   //Thoát khỏi vòng lặp
+                        }
+                    }
+                    if (!isExist)   //Nếu file chưa tồn tại trong danh sách
+                    {
+                        DLL.AddSong(filePaths[i], fileNames[i]);   //Thêm file vào danh sách
+                        ListSong.Items.Add(fileNames[i]);   //Hiện thị tên file trong danh sách
+                    }
+                    else   //Nếu file đã tồn tại trong danh sách
+                        MessageBox.Show($"File \"{fileNames[i]}\" đã tồn tại trong danh sách!", "Trùng lặp", MessageBoxButtons.OK, MessageBoxIcon.Warning);   //Thông báo file đã tồn tại
+                }
+            }
+        }
+        private void btn_Remove_Click(object sender, EventArgs e)
+        {
+            if (ListSong.SelectedIndex != -1)   // Nếu đã chọn bài hát
+            {
+                int selectedIndex = ListSong.SelectedIndex;   // Lấy vị trí bài hát được chọn
+                DLL.RemoveAt(selectedIndex);   // Xóa bài hát khỏi danh sách
+                ListSong.Items.RemoveAt(selectedIndex);   // Xóa bài hát khỏi ListBox
+                MessageBox.Show("Đã xóa bài hát thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);   // Thông báo xóa thành công
+            }
+            else MessageBox.Show("Vui lòng chọn một bài hát để xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);   // Thông báo chưa chọn bài hát
+        }
+        private void btn_Swap_Click(object sender, EventArgs e)
+        {
+            if(ListSong.SelectedIndices.Count != 2)   // Nếu chưa chọn đúng 2 dòng(bài hát)
+            {
+                MessageBox.Show("Vui lòng chọn 2 bài hát để hoán đổi vị trí!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);   // Thông báo chưa chọn 2 bài hát
+                return;   // Thoát khỏi phương thức
+            }
+            int index1 = ListSong.SelectedIndices[0];   // Lấy vị trí bài hát đầu tiên
+            int index2 = ListSong.SelectedIndices[1];   // Lấy vị trí bài hát thứ hai
+            DLL.SwapSong(index1, index2);   // Hoán đổi vị trí 2 bài hát
+            RefreshListSong(index1, index2);   // Cập nhật danh sách
+            MessageBox.Show("Đã hoán đổi vị trí thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);   // Thông báo hoán đổi thành công
+        }
+        private void ListSong_DoubleClick(object sender, EventArgs e)
+        {
+            int selectedIndex = ListSong.SelectedIndex;   // Lấy vị trí bài hát được chọn
+            DLL.Current = DLL.Head;   // Khởi tạo Node hiện tại là Node đầu tiên
+            for (int i = 0; i < selectedIndex; i++)   // Duyệt qua các Node trong danh sách
+                DLL.Current = DLL.Current.Next;   // Chuyển sang Node tiếp theo
+            axWindowsMediaPlayer.URL = DLL.Current.FilePath;   // Gán đường dẫn bài hát cho Media Player
+            axWindowsMediaPlayer.Ctlcontrols.play();   // Phát bài hát
+            btnPlay.BackgroundImage = Properties.Resources.pause;
+        }
+        private bool isButtonClick = false;   // Biến kiểm tra trạng thái nút nhấn
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            isButtonClick = true; // Đánh dấu trạng thái được thay đổi bởi nút nhấn
+
+            if (axWindowsMediaPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                // Tạm dừng nhạc và đổi biểu tượng sang Play
+                axWindowsMediaPlayer.Ctlcontrols.pause();
+                btnPlay.BackgroundImage = Properties.Resources.play;
+                //timer.Stop();
+            }
+            else
+            {
+                // Phát nhạc và đổi biểu tượng sang Pause
+                axWindowsMediaPlayer.Ctlcontrols.play();
+                btnPlay.BackgroundImage = Properties.Resources.pause;
+                //timer.Start();
+            }
+            btnPlay.Refresh(); // Cập nhật giao diện
+            isButtonClick = false; // Hoàn thành xử lý nút nhấn
+        }
+
+        private void RefreshListSong(int index1, int index2)   
+        {
+            ListSong.Items.Clear();   // Xóa danh sách hiện tại
+            SongNode current = DLL.Head;   // Khởi tạo Node hiện tại là Node đầu tiên
+            while (current != null)   // Duyệt qua các Node trong danh sách
+            {
+                ListSong.Items.Add(current.FileName);   // Thêm tên bài hát vào danh sách
+                current = current.Next;   // Chuyển sang Node tiếp theo
+            }
+            ListSong.SelectedIndices.Add(index1);   //Đánh dấu chọn bài hát đầu tiên
+            ListSong.SelectedIndices.Add(index2);   //Đánh dấu chọn bài hát thứ hai
+        }
+    }
+}
